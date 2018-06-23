e1522ecf3abefe725045a191d89a014c50cbda86
RashidMall <mr.mallaev@gmail.com>
2018-06-23 16:35:33 +0200

    sql-db

--- SQL/blog.sql
+++ SQL/blog.sql
@@ -1,29 +1,31 @@
+-- CREATE DATABASE cinema CHARACTER SET 'utf8';
+set foreign_key_checks=0;
+
 CREATE TABLE `User` (
     `ID_user` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(30) NOT NULL,
-    `pw` TEXT NOT NULL,
-    `mail` TEXT NOT NULL,
-    PRIMARY KEY (`id_user`)
-);
+    `user_name` VARCHAR(30) NOT NULL,
+    `user_pw` TEXT NOT NULL,
+    `user_mail` TEXT NOT NULL,
+    PRIMARY KEY (`ID_user`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 CREATE TABLE `Post` (
     `ID_post` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(50) NOT NULL,
-    `post` TEXT NOT NULL,
-    `date_post` DATETIME NOT NULL,
-    `author_post` VARCHAR(30) NOT NULL,
-    PRIMARY KEY (`id_post`)
-);
+    `post_title` VARCHAR(50) NOT NULL,
+    `post_content` TEXT NOT NULL,
+    `post_date` DATETIME NOT NULL,
+    `idUser` INT UNSIGNED,
+    CONSTRAINT fk_UserPost FOREIGN KEY (idUser) REFERENCES User(ID_user),
+    PRIMARY KEY (`ID_post`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 CREATE TABLE `Comment` (
     `ID_comment` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `comment` TEXT NOT NULL,
-    `post_id` INT,
+    `comment_content` TEXT NOT NULL,
     `date_comment` DATETIME NOT NULL,
-    `author_comment` VARCHAR(30) NOT NULL,
-    PRIMARY KEY (`id_comment`)
-);
-
-ALTER TABLE `Post` ADD CONSTRAINT FOREIGN KEY (`author_post`) REFERENCES `User`(`ID_user`);
-ALTER TABLE `Comment` ADD CONSTRAINT FOREIGN KEY (`author_comment`) REFERENCES `User`(`ID_user`);
-ALTER TABLE `Comment` ADD CONSTRAINT FOREIGN KEY (`post_id`) REFERENCES `Post`(`ID_post`);
+    `idPost` INT UNSIGNED,
+    `idUser` INT UNSIGNED,
+    CONSTRAINT fk_PostComment FOREIGN KEY (idPost) REFERENCES Post(ID_post),
+    CONSTRAINT fk_UserComment FOREIGN KEY (idUser) REFERENCES User(ID_user),
+    PRIMARY KEY (`ID_comment`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
