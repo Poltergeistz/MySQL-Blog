f76cad82d3000bdb39f2b029a60f45324010f734
david <david.journo.31@gmail.com>
2018-06-24 16:22:46 +0200

    insert query with error handling

--- insert.js
+++ insert.js
@@ -10,4 +10,18 @@ var db = mysql.createConnection({
 });
 db.connect();
 
-//
+// Insert data into database
+
+var Post = {
+
+};
+// Show elements inserted into db
+var query = db.query('Inserted into database : ?', Post, function(err, result){
+    // Error log
+    if(err){
+        console.error(err);
+        return;
+    }
+    // Else execute query
+    console.log(result);
+});
