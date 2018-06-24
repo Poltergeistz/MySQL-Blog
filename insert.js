a2496c6b634fb0c675a7e803c2fc264e7f62df8c
david <david.journo.31@gmail.com>
2018-06-24 16:02:34 +0200

    insert data into db

---
+++ insert.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+// Create a connection into the database
+var db = mysql.createConnection({
+    host    : "sql7.freemysqlhosting.net",
+    port    : "3306",
+    user    : "sql7244055",
+    password: "Ee5FBQbYYB",
+    database: "sql7244055"
+});
+db.connect();
+
+//
