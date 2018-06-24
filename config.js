2aadb0b4dc5945dbe4390e2b774f157f1016c1f3
david <david.journo.31@gmail.com>
2018-06-24 17:36:56 +0200

    config module

---
+++ config.js
@@ -0,0 +1,10 @@
+// Config file export module
+var config = {
+    host    : "sql7.freemysqlhosting.net",
+    port    : "3306",
+    user    : "sql7244055",
+    password: "Ee5FBQbYYB",
+    database: "sql7244055"
+};
+
+module.exports = config;
